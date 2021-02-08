eec0521456f7261c7a23f6135743c093ef635363
Elia Cools <eliacools@hotmail.com>
2021-02-08 20:47:46 +0100

    added evoluations per pokemon

--- pokedex.js
+++ pokedex.js
@@ -5,6 +5,7 @@ let idParagraph = document.createElement("p");
 let movesParagraph= document.createElement("p");
 let evolvedfromParagraph= document.createElement("p");
 let nameParagraph = document.createElement("p");
+let evolutionparagraph=document.createElement("p");
 
 document.getElementById('run').addEventListener('click',function(){
 
@@ -14,15 +15,15 @@ document.getElementById('run').addEventListener('click',function(){
         let data = await fetch('https://pokeapi.co/api/v2/pokemon/'+input);
         let pokemon = await data.json();
 
+
         image.setAttribute("src",pokemon.sprites.front_default)
-        
 
         document.getElementById("pokedexscreen").appendChild(image)
         document.getElementById("pokedexscreen").appendChild(nameParagraph)
         document.getElementById("pokedexscreen").appendChild(idParagraph)
         document.getElementById("pokedexscreen").appendChild(movesParagraph)
         document.getElementById("pokedexscreen").appendChild(evolvedfromParagraph)
-
+        document.getElementById("pokedexscreen").appendChild(evolutionparagraph)
 
 
         var newarray=[]
@@ -39,6 +40,8 @@ document.getElementById('run').addEventListener('click',function(){
 
 
         let speciesurl = await fetch('https://pokeapi.co/api/v2/pokemon-species/'+pokemon.id)
+
+
         let speciesdata= await speciesurl.json()
 
 
@@ -50,14 +53,26 @@ document.getElementById('run').addEventListener('click',function(){
         }
 
 
-
-
         let evolutionchain=await fetch(speciesdata.evolution_chain.url)
         let evolutiondata = await evolutionchain.json();
 
-        console.log(evolutiondata.chain.evolves_to[0])
+        let lastevoluation = await fetch('https://pokeapi.co/api/v2/pokemon-species/'+evolutiondata.chain.evolves_to[0].species.name)
+        let lastspecie= await lastevoluation.json()
+        let firstspecie= lastspecie.evolves_from_species.name
 
+        let firstEvoluation=evolutiondata.chain.evolves_to[0].species.name
 
+             if(evolutiondata.chain.evolves_to[0].evolves_to[0]!==undefined){
+            let secondEvoluation= evolutiondata.chain.evolves_to[0].evolves_to[0].species.name
+           evolutionparagraph.innerText=firstspecie+","+firstEvoluation+","+secondEvoluation
+             }else{
+                 evolutionparagraph.innerText=firstspecie+","+firstEvoluation
+             }
     }
+
+
     pokedex();
+
+
+
 })
