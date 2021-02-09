be810857abcf6c7c60541f6eac0a183a31259bb0
Elia Cools <eliacools@hotmail.com>
2021-02-09 11:22:51 +0100

    Added the evolution pictures

--- pokedex.js
+++ pokedex.js
@@ -7,60 +7,43 @@ let evolvedfromParagraph= document.createElement("p");
 let nameParagraph = document.createElement("p");
 let evolutionparagraph=document.createElement("p");
 
-document.getElementById("screen").appendChild(image)
-document.getElementById("screen").appendChild(nameParagraph)
-document.getElementById("screen").appendChild(idParagraph)
-document.getElementById("screen").appendChild(movesParagraph)
-document.getElementById("screen").appendChild(evolvedfromParagraph)
-document.getElementById("screen").appendChild(evolutionparagraph)
-for (let i = 0; i <document.querySelectorAll('button').length ; i++) {
+document.getElementById('run').addEventListener('click',function(){
 
 
-document.querySelectorAll('button')[i].addEventListener('click',function(){
-
-
-
-
-    let input = document.getElementById('input').value
-    async function pokedex() {
-
-        let data = await fetch('https://pokeapi.co/api/v2/pokemon/' + input);
+    async function pokedex(){
+        let input = document.getElementById('input').value
+        let data = await fetch('https://pokeapi.co/api/v2/pokemon/'+input);
         let pokemon = await data.json();
 
-        image.setAttribute("src", pokemon.sprites.front_default)
-        image.setAttribute("id", "sprites")
 
+        image.setAttribute("src",pokemon.sprites.front_default)
+        image.setAttribute("id","sprites")
 
-
-        var newarray = []
-        for (let j = 0; j < pokemon.moves.length; j++) {
-            if (j <= 5) {
-                newarray.push(pokemon.moves[j].move.name)
+        document.getElementById("screen").appendChild(image)
+        document.getElementById("screen").appendChild(nameParagraph)
+        document.getElementById("screen").appendChild(idParagraph)
+        document.getElementById("screen").appendChild(movesParagraph)
+        document.getElementById("screen").appendChild(evolvedfromParagraph)
+        document.getElementById("screen").appendChild(evolutionparagraph)
+        var newarray=[]
+        for (let i = 0; i < pokemon.moves.length; i++) {
+            if(i<=5) {
+                newarray.push(pokemon.moves[i].move.name)
             }
         }
 
-        nameParagraph.innerText = pokemon.name.charAt();
-        idParagraph.innerText = "ID: " + pokemon.id
-        movesParagraph.innerText = "moves: " + newarray.join(",  ");
-
-        async function evolvedfrom() {
-            let speciesurl = await fetch('https://pokeapi.co/api/v2/pokemon-species/' + pokemon.id)
-            let speciesdata = await speciesurl.json()
-
-            if (speciesdata.evolves_from_species === null) {
-                evolvedfromParagraph.innerText = "no prior evoluation"
-            } else {
-                evolvedfromParagraph.innerText = "evolved from: " + speciesdata.evolves_from_species.name
-            }
-            let evolutionchain = await fetch(speciesdata.evolution_chain.url)
-            let evolutiondata = await evolutionchain.json();
+        nameParagraph.innerText=pokemon.name;
+        idParagraph.innerText="ID: "+pokemon.id
+        movesParagraph.innerText="moves: "+newarray.join(",  ");
 
 
 
-            //maybe remove this, this part of the code is to get all the possible evoluations of a given pokémon. doesn't work with variations
-            let lastevoluation = await fetch('https://pokeapi.co/api/v2/pokemon-species/' + evolutiondata.chain.evolves_to[0].species.name)
-            let lastspecie = await lastevoluation.json()
-            let firstspecie = lastspecie.evolves_from_species.name
+        let speciesurl = await fetch('https://pokeapi.co/api/v2/pokemon-species/'+pokemon.id)
+
+
+        let speciesdata= await speciesurl.json()
+
+
 
         if(speciesdata.evolves_from_species===null){
             evolvedfromParagraph.innerText="no prior evolution"
@@ -69,30 +52,75 @@ document.querySelectorAll('button')[i].addEventListener('click',function(){
         }
 
 
-            let firstEvoluation = evolutiondata.chain.evolves_to[0].species.name
+        let evolutionchain=await fetch(speciesdata.evolution_chain.url)
+        let evolutiondata = await evolutionchain.json();
 
-            if (evolutiondata.chain.evolves_to[0].evolves_to[0] !== undefined) {
-                let secondEvoluation = evolutiondata.chain.evolves_to[0].evolves_to[0].species.name
-                evolutionparagraph.innerText = firstspecie + "," + firstEvoluation + "," + secondEvoluation
-            } else {
-                evolutionparagraph.innerText = firstspecie + "," + firstEvoluation
-            }
-        }
-       await evolvedfrom();
+        document.getElementById('first-pokemon').innerHTML="";
+        document.getElementById('second-pokemon').innerHTML="";
+        document.getElementById('third-pokemon').innerHTML="";
+       if(evolutiondata.chain.evolves_to[0]!==undefined){
+
+
+
+        let lastevolution = await fetch('https://pokeapi.co/api/v2/pokemon-species/'+evolutiondata.chain.evolves_to[0].species.name)
+        let lastspecie= await lastevolution.json()
+
+        let firstEvolution= lastspecie.evolves_from_species.name
+        let secondEvolution=evolutiondata.chain.evolves_to[0].species.name
+
+
+        let firstevolutionpicture = await fetch('https://pokeapi.co/api/v2/pokemon/'+firstEvolution);
+        let pokemon1 = await firstevolutionpicture.json();
+
+        let secondevolutionpicture = await fetch('https://pokeapi.co/api/v2/pokemon/'+secondEvolution);
+        let pokemon2 = await secondevolutionpicture.json();
+
+
+
+
+
+
+        let firstevolutionimage = document.createElement('img')
+        firstevolutionimage.setAttribute('src',pokemon1.sprites.front_default)
+        firstevolutionimage.setAttribute('id','first-evolution')
+
+     let secondevolutionimage = document.createElement('img')
+        secondevolutionimage.setAttribute('src',pokemon2.sprites.front_default)
+        secondevolutionimage.setAttribute('id','second-evolution')
+
+
+
+
+
+
+
+
+             if(evolutiondata.chain.evolves_to[0].evolves_to[0]!==undefined){
+                 let thirdEvolution= evolutiondata.chain.evolves_to[0].evolves_to[0].species.name
+
+                 let thirdevolutionpicture = await fetch('https://pokeapi.co/api/v2/pokemon/'+thirdEvolution);
+                 let pokemon3 = await thirdevolutionpicture.json();
+
+                 let thirdevolutionimage = document.createElement('img')
+                 thirdevolutionimage.setAttribute('src',pokemon3.sprites.front_default)
+                 thirdevolutionimage.setAttribute('id','third-evolution')
+
+                document.getElementById('first-pokemon').appendChild(firstevolutionimage)
+                document.getElementById('second-pokemon').appendChild(secondevolutionimage)
+                document.getElementById('third-pokemon').appendChild(thirdevolutionimage)
+
+
+
+             }else if(evolutiondata.chain.evolves_to[0]!==undefined){
+                 document.getElementById('first-pokemon').appendChild(firstevolutionimage)
+                 document.getElementById('second-pokemon').appendChild(secondevolutionimage)
+             }
+       }
     }
 
-    if(document.querySelectorAll('button')[i]===document.getElementById('next')){
 
-    }
-
-
-    if(document.querySelectorAll('button')[i]===document.getElementById('run')){
-        pokedex();
-
-    }
+    pokedex();
 
 
 
 })
-
-}
